1d5f34340f617cd1db65878ff9631001f6aeeeec
Muhammed AKYILDIZ <muhammedakyildiz88@gmail.com>
2025-11-26 23:37:57 +0300

    deneme

--- SporSalonuSolution/SporSalonu.Desktop/Form1.cs
+++ SporSalonuSolution/SporSalonu.Desktop/Form1.cs
@@ -9,7 +9,7 @@ namespace SporSalonu.Desktop
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "OK";
+            textBox1.Text = "OKAY";
         }
     }
 }
