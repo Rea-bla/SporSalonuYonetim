cf0d98bf16f7114405ad9e970af2815142a396e6
Furkan <furkangltkn58@gmail.com>
2025-11-27 22:46:16 +0300

    Tools

--- SporSalonuSolution/SporSalonu.Desktop/Form1.Designer.cs
+++ SporSalonuSolution/SporSalonu.Desktop/Form1.Designer.cs
@@ -28,56 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            pictureBox1 = new PictureBox();
-            textBox1 = new TextBox();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(422, 191);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(55, 20);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(264, 273);
-            pictureBox1.TabIndex = 1;
-            pictureBox1.TabStop = false;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(410, 133);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(pictureBox1);
-            Controls.Add(button1);
+            ClientSize = new Size(1115, 619);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Button button1;
-        private PictureBox pictureBox1;
-        private TextBox textBox1;
     }
 }
