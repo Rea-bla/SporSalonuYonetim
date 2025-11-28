b2fee9450b67dcf04fa4346833d062ad1c3e2bb5
Furkan <furkangltkn58@gmail.com>
2025-11-29 00:25:09 +0300

    HomePage

---
+++ SporSalonuSolution/SporSalonu.Desktop/HomePage.cs
@@ -0,0 +1,26 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SporSalonu.Desktop
+{
+    public partial class HomePage : Form
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Anasayfa_Load(object sender, EventArgs e)
+        {
+            guna2ShadowForm1.SetShadowForm(this);
+        }
+    }
+}
