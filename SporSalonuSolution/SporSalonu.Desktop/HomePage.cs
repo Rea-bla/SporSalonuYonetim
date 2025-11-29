b0c55073c151edf6f81f0fda51083b97c396d750
Furkan <furkangltkn58@gmail.com>
2025-11-29 21:57:47 +0300

    Anasayfa tuş ayarları

--- SporSalonuSolution/SporSalonu.Desktop/HomePage.cs
+++ SporSalonuSolution/SporSalonu.Desktop/HomePage.cs
@@ -21,6 +21,7 @@ namespace SporSalonu.Desktop
         private void Anasayfa_Load(object sender, EventArgs e)
         {
             guna2ShadowForm1.SetShadowForm(this);
+            container(new Dashboard());
         }
 
         private void guna2Button5_Click(object sender, EventArgs e)
@@ -28,6 +29,23 @@ namespace SporSalonu.Desktop
             this.Close();
         }
 
-       
+        private void container(object _form)
+        {
+            if (guna2PanelA_container.Controls.Count > 0) guna2PanelA_container.Controls.Clear();
+
+            Form fm = _form as Form;
+            fm.TopLevel = false;
+            fm.FormBorderStyle = FormBorderStyle.None;
+            fm.Dock = DockStyle.Fill;
+            guna2PanelA_container.Controls.Add(fm);
+            guna2PanelA_container.Tag = fm;
+            fm.Show();
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            container(new Dashboard());
+        }
     }
 }
